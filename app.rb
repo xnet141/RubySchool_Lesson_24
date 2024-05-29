4c1de724c53f43de4ef40fe9c3aaacc4b597ae6e
Alex Erofeev <xnet141@gmail.com>
2024-05-30 00:37:36 +0300

    My test pony - test parameters

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
 end
@@ -28,13 +29,58 @@ get '/login' do
 	erb :login
 end
 
+get '/cont' do
+	haml :cont
+end
+
+
+
 post '/contacts' do
-	@email = params[:email]
-	@textarea     = params[:textarea]
-	f = File.open 'public/contacts.txt', 'a' #режим 'а' значит append, то есть добавить в конец файла
-	f.write "\nemail: #{@email}, message: #{@textarea}<br>"
-	f.close
-	erb :contacts
+	require 'pony'
+	# Pony.mail(
+	#    :name => params[:name],
+	#   :mail => params[:mail],
+	#   :body => params[:body],
+	#   :to => 'mh101@yandex.com',
+	#   :subject => params[:name] + " has contacted you",
+	#   #:body => params[:message],
+	#   :port => '465',
+	#   :via => :smtp,
+	#   :via_options => { 
+	#     :address              => 'smtp.yandex.com', 
+	#     :port                 => '465', 
+	#     :enable_starttls_auto => true, 
+	#     :user_name            => 'mh101@yandex.com', 
+	#     :password             => 'Lv-426-napoval9933', 
+	#     :authentication       => :plain, 
+	#     :domain               => 'localhost.localdomain'
+  	# })
+
+	Pony.mail(
+	  	:to => 'xnet141@gmail.com', 
+	  	:from => 'xnet141@gmail.com',
+	  	:subject => 'test', 
+	  	:body => "test pony", 
+	  	:via => :smtp, 
+	  	:via_options => {
+	      :address     => 'smtp.gmail.com',
+	      :port                 => '587',
+    :enable_starttls_auto => true,
+    :user_name            => 'xnet141',
+    :password             => 'zwke kkvl svbv jlsr',
+    :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+    :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+    	}
+	)	
+
+
+	#@email = params[:email]
+	#@textarea     = params[:textarea]
+
+	#f = File.open 'public/contacts.txt', 'a' #режим 'а' значит append, то есть добавить в конец файла
+	#f.write "\nemail: #{@email}, message: #{@textarea}<br>"
+	#f.close
+	redirect '/cont'
 end
 
 post '/visit' do
@@ -43,7 +89,7 @@ post '/visit' do
 	@datetime = params[:datetime]
 	@barber = params[:barber]
 	@color = params[:color]
-	#@error = ''
+	
 	# хеш
 	hh = {  :username => 'Введите имя!!',
 			:phone => 'Введите телефон',
@@ -68,3 +114,4 @@ post '/login' do
 		erb :login
 	end
 end
+
